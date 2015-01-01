8ced1e34622e2f103613a1c7cd7f9d77836eb544
Diana Krasnozhenova <dianakrasnozhenova@MacBook-Air-Diana.local>
2015-01-01 14:44:13 +0300

    Initial Commit

---
+++ FlappyGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlappyGame
+//
+//  Created by Diana Krasnozhenova on 1/1/15.
+//  Copyright (c) 2015 Diana Krasnozhenova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
